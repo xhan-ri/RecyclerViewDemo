3b8c769e145710cd468c2377d4aaa749c8fbc22b
xhan-ri <xhan@rhapsody.com>
2016-02-11 08:53:30 -0800

    add different layout manager

--- app/src/main/java/com/rhapsody/xhan/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/rhapsody/xhan/recyclerviewdemo/MainActivity.java
@@ -3,16 +3,24 @@ package com.rhapsody.xhan.recyclerviewdemo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.rhapsody.xhan.recyclerviewdemo.view.DemoAdapter;
+import com.xiaofeng.flowlayoutmanager.FlowLayoutManager;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 	private static final int MAX_SIZE = 10;
 	private static final String STRING_BASE = "0123456789";
 	static Random random = new Random();
+	Spinner layoutSpinner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,19 +47,18 @@ public class MainActivity extends AppCompatActivity {
 			@Override
 			public void onClick(View view) {
 				recyclerView.setAdapter(new DemoAdapter(generateDemoItems(50)));
-				recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this) {
-					@Override
-					public boolean supportsPredictiveItemAnimations() {
-						CheckBox supportPredictiveAnimationCheck = (CheckBox)findViewById(R.id.support_predictive_animation);
-						return supportPredictiveAnimationCheck.isChecked();
-					}
-				});
+				CheckBox supportPredictiveAnimationCheck = (CheckBox)findViewById(R.id.support_predictive_animation);
+				recyclerView.setLayoutManager(createLayoutManager(supportPredictiveAnimationCheck.isChecked()));
 			}
 		});
 		recyclerView.getItemAnimator().setAddDuration(3000);
 		recyclerView.getItemAnimator().setRemoveDuration(3000);
 		recyclerView.getItemAnimator().setChangeDuration(3000);
 		recyclerView.getItemAnimator().setChangeDuration(3000);
+
+		ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.layouts, android.R.layout.simple_spinner_dropdown_item);
+		layoutSpinner = (Spinner)findViewById(R.id.layouts);
+		layoutSpinner.setAdapter(spinnerAdapter);
 	}
 
 	@Override
@@ -125,5 +133,40 @@ public class MainActivity extends AppCompatActivity {
 		View lastView = recyclerView.getLayoutManager().getChildAt(recyclerView.getLayoutManager().getChildCount() - 1);
 		int lastItemAdapterIndex = recyclerView.getChildAdapterPosition(lastView);
 		return firstItemAdapterIndex + (lastItemAdapterIndex - firstItemAdapterIndex) / 2;
+	}
+
+	private RecyclerView.LayoutManager createLayoutManager(final boolean supportPredictiveAnimation) {
+		String layoutName = layoutSpinner.getSelectedItem().toString();
+		RecyclerView.LayoutManager layoutManager = null;
+		if (layoutName.equals("Linear")) {
+			layoutManager = new LinearLayoutManager(this) {
+				@Override
+				public boolean supportsPredictiveItemAnimations() {
+					return supportPredictiveAnimation;
+				}
+			};
+		} else if (layoutName.equals("Grid")) {
+			layoutManager = new GridLayoutManager(this, 3) {
+				@Override
+				public boolean supportsPredictiveItemAnimations() {
+					return supportPredictiveAnimation;
+				}
+			};
+		} else if (layoutName.equals("Stagger")) {
+			layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.HORIZONTAL) {
+				@Override
+				public boolean supportsPredictiveItemAnimations() {
+					return supportPredictiveAnimation;
+				}
+			};
+		} else if (layoutName.equals("Flow")) {
+			layoutManager = new FlowLayoutManager() {
+				@Override
+				public boolean supportsPredictiveItemAnimations() {
+					return supportPredictiveAnimation;
+				}
+			};
+		}
+		return layoutManager;
 	}
 }
